6a440cde3ee75614547cf11e0b82fa51404cd271
Casey Jacuzzi <ccjacuzzi@gmail.com>
2023-03-16 19:15:10 -0500

    Starting on java.

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -22,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mBinding.getRoot();
         setContentView(view);
+
+        this.mBinding.CalculateTotalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*call "calculateTotal"*/
+                calculateTotal();
+            }
+        });
+    }
+
+    private String calculateTotal(){
+       float subTotal = 0;
+       String total = "The 'calculateTotal' method is under construction.";
+
+       /* If invoice checkbox 1 is checked, then get text from invoice textinput 1. Convert it to float. Store value in subTotal. Repeat for checkboxes 2-4. */
+       /* Once all values are added to subTotal, convert subTotal from float to a string and store in variable called "total".*/
+
+       return total;
     }
 }
